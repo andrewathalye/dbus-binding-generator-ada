b1775e1932b9f7d4d0e95abd922204a105a93a81
Andrew Athalye <andrewathalye@outlook.com>
2024-08-03 01:26:52 -0400

    Fix nix packaging

--- nix/default.nix
+++ nix/default.nix
@@ -11,8 +11,10 @@
 
 stdenv.mkDerivation rec {
    pname = "dbus-binding-generator-ada";
-   version = "1.0";
+   version = "1.0.1";
    src = ./..;
+
+   outputs = ["out" "lib" "dev"];
 
    dbus-binding-generator-ada-schema = ../share/introspect.xsd;
    
@@ -42,8 +44,8 @@ stdenv.mkDerivation rec {
       export BUILD_MODE=prod
       export LIBRARY_TYPE=relocatable
 
-      gprinstall -m -p -Pdbus_binding_generator_ada --prefix=$out --mode=usage --no-project --no-manifest
-      gprinstall -m -p -Psupportlib/dbusada_support.gpr --prefix=$out --mode=dev --no-manifest
+      gprinstall -p -Pdbus_binding_generator_ada --prefix=$out --mode=usage --no-project --no-manifest
+      gprinstall -m -p -Psupportlib/dbusada_support.gpr --prefix=$dev --lib-subdir=$lib/lib --mode=dev --no-manifest
 
       runHook postInstall
    '';
